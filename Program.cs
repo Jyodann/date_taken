635f210f295734bdad916a60fe4d3f306454adf8
Jordan <jordynwinnie@gmail.com>
2023-10-14 22:23:45 +0800

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,145 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Globalization;
+using MetadataExtractor;
+using MetadataExtractor.Formats.Exif;
+
+internal class Program
+{
+	private static void Main(string[] args)
+	{
+		Console.Write("Enter the FULL directory of where your unsorted photos are: ");
+		var fileDirectory = Console.ReadLine();
+
+		if (!System.IO.Directory.Exists(fileDirectory))
+		{
+			Console.WriteLine("Not a valid directory");
+			return;
+		}
+		Console.Write("Enter the FULL directory of where you want the sorted items to be: ");
+
+		var workingDirectory = Console.ReadLine();
+		if (!System.IO.Directory.Exists(workingDirectory))
+		{
+			Console.WriteLine("Not a valid directory");
+			return;
+		}
+
+		Console.Write("Do you want to replace duplicates? (y/n): ");
+		var replaceDuplicateOption = Console.ReadLine();
+        bool replaceDuplicates;
+        switch (replaceDuplicateOption)
+		{
+			case "y":
+				replaceDuplicates = true;
+				break;
+			case "n":
+				replaceDuplicates = false;
+				break;
+			default:
+				Console.WriteLine("Invalid Option.");
+				Console.ReadKey();
+				return;
+		}
+
+		Console.WriteLine("Discovering Directories...");
+
+		var allFiles = ReturnAllFiles(fileDirectory);
+		Console.WriteLine($"Found {allFiles.Count} files. Press any key to continue with the move.");
+		Console.ReadKey();
+		var unsortedDirectory = $@"{workingDirectory}\unsorted_items";
+		var filesWithIssues = $@"{workingDirectory}\files_with_issues";
+
+
+		System.IO.Directory.CreateDirectory(unsortedDirectory);
+		System.IO.Directory.CreateDirectory(filesWithIssues);
+
+		Console.WriteLine($"Discovered {allFiles.Count} Files");
+		foreach (var file in allFiles)
+		{
+			var fileName = Path.GetFileName(file);
+			var finalFileDirectory = "";
+
+			try
+			{
+				var directories = ImageMetadataReader.ReadMetadata(file);
+				var success = ReturnDateTime(directories, out var date);
+
+				if (success)
+				{
+					var directoryFriendlyDate = date.ToString("dd-MM-yyyy");
+					var parentDirectory = $@"{workingDirectory}\{directoryFriendlyDate}";
+					System.IO.Directory.CreateDirectory(parentDirectory);
+					finalFileDirectory = $@"{parentDirectory}\{fileName}";
+				}
+				else
+				{
+					Console.WriteLine($"File {fileName} has no date");
+					finalFileDirectory = $@"{unsortedDirectory}\{fileName}";
+				}
+
+				
+			}
+			catch (ImageProcessingException)
+			{
+				Console.WriteLine($"File {fileName} has an unrecognised format");
+				finalFileDirectory = $@"{filesWithIssues}\{fileName}";
+			}
+			catch (IndexOutOfRangeException)
+			{
+				Console.WriteLine($"File {fileName} has an unrecognised format");
+				finalFileDirectory = $@"{filesWithIssues}\{fileName}";
+			}
+
+			if (File.Exists(finalFileDirectory)) {
+				if (replaceDuplicates) {
+					Console.WriteLine($"File {fileName} is a duplicate. Replacing...");
+				} else {
+					var guid = Guid.NewGuid().ToString();
+					Console.WriteLine($"File {fileName} is a duplicate. Appending {guid} to the start of filename");
+					finalFileDirectory = $@"{filesWithIssues}\{guid}-{fileName}";
+				}
+			}
+			File.Move(file, finalFileDirectory, replaceDuplicates);
+		}
+
+		Console.WriteLine("Done!");
+		Console.ReadKey();
+		bool ReturnDateTime(IReadOnlyList<MetadataExtractor.Directory> directories, out DateOnly date)
+		{
+
+			date = DateOnly.MinValue;
+			foreach (var item in directories)
+			{
+				foreach (var tag in item.Tags)
+				{
+					if (tag.Type == ExifDirectoryBase.TagDateTimeOriginal)
+					{
+						date = DateOnly.FromDateTime(DateTime.ParseExact(tag.Description!, "yyyy:MM:dd HH:mm:ss", CultureInfo.InvariantCulture));
+						return true;
+					}
+				}
+			}
+
+			return false;
+		}
+
+		List<string> ReturnAllFiles(string path)
+		{
+			var finalList = new List<string>();
+			var folders = System.IO.Directory.EnumerateFileSystemEntries(path);
+			foreach (var file in folders)
+			{
+				if (System.IO.Directory.Exists(file))
+				{
+					finalList.AddRange(ReturnAllFiles(file));
+					continue;
+				}
+				if (File.Exists(file))
+				{
+					finalList.Add(file);
+				}
+			}
+			return finalList;
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,74 @@
+# Date Taken
+
+```
+!!! NOTE: ALWAYS BACKUP YOUR FILES BEFORE DOING ANY OPERATIONS ON THEM. I am not responsible for any data losses due to the usage of this application. 
+```
+
+A C# Console application that sorts images by the date they were taken/created.
+
+Sometimes, when you export from Major Cloud Providers, or have multiple sources of photos from devices, your directory can look like this:
+
+```
+top_level
+├───Camera Roll
+├───Google Backup
+│   ├───takeout-20231011T134603Z-001
+│   │   └───Takeout
+│   │       └───Google Photos
+│   │           ├───Photos from 2022
+│   │           ├───Photos from 2023
+│   │           ├───Taiwan Trip 
+│   │           └───Trash
+│   ├───takeout-20231011T134603Z-002
+│   │   └───Takeout
+│   │       └───Google Photos
+│   │           ├───Photos from 2020
+│   │           ├───Photos from 2021
+├───Screenshots
+├───Whatsapp
+└───WhatsApp Images
+```
+
+When viewing photos, most of the time you are concerned with the moment where the memory occured. This tool will reorganise all the photos according to Day so your directory looks like this:
+
+```
+top_level
+├───unsorted_items
+├───files_with_issues
+├───22-08-2023
+├───14-08-2023
+├───04-09-2023
+├───13-08-2023
+├───20-08-2023
+```
+
+The application uses the EXIF format information tied to your photo to attempt to sort the photo. If it cannot sort the photo, it goes into the `unsorted_items` folder. Any erroneous files or unsupported formats goes into the `files_with_issues` folder.
+
+## Usage
+
+```
+PLEASE NOTE THAT FILES WITH THE SAME NAME ARE OVERWRITTEN IF THEY HAPPEN TO FALL ON THE SAME DATE
+```
+
+Double Click the .exe file, and a Command prompt / Terminal will appear. Type in the FULL path of where you want to look for files. And where you would like to move your files. 
+
+An example of how a FULL path looks like: 
+
+`E:\Jordan Photos`
+
+The application will search through for all the files in the directory you provided and ask for confirmation before moving on. 
+
+If the file number does not look right, please press `Ctrl+C` to exit the application.
+
+Upon confirmation, the application will start moving the files. This may take anywhere from a few minutes to a few hours, depending o nthe number of photos you have.
+
+When done, the application will write "Done!"
+
+## Technical Information
+
+Dependencies: 
+
+- .NET 7
+- MetadataExtractor
+
+To set up, just clone this directory, and use `dotnet run`, it should install all dependencies for you.
